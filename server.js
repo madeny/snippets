5701b2ce6db61eeb5cbdc2167c424282170b8817
Madeny Diawara <madeny@live.com>
2016-10-18 00:16:52 +0200

    Auto updates

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 var express = require('express'),
 	app 	= express();
 
+
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res) {
-	res.send('hello me');
+	// res.send('index.html');
+	res.render('index');
+
     
 });
+
 
 app.listen(3000, function () {
 	console.log('ReadyUp');
