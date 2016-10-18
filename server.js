14850fe3907be24bd086e1e643eb06345db30445
Madeny Diawara <madeny@live.com>
2016-10-18 21:04:00 +0200

    Auto updates

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 var express = require('express'),
 	app 	= express();
-
+	messagebird = require('messagebird')('live_FyFTz0kVi5aVUJaagMA0vBGmB');
 
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
-app.get('/',function(req,res) {
-	// res.send('index.html');
-	res.render('index');
 
-    
-});
+
+  app.get('/',function(req,res) {
+   messagebird.balance.read(function (err, response) {
+  if (err) {
+   console.log(err);
+  } else
+       res.render('index',{balance:response.amount});
+    });   
+ });
+
 
 
 app.listen(3000, function () {
