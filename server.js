dc8cfe583e079c806a4379260e32ec2d9c79834f
Madeny Diawara <madeny@Ensure.local>
2016-10-25 03:08:18 +0200

    Auto updates

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
 
 
+
+
+
+
  app.get('/',function(req,res) {
    messagebird.balance.read(function (err, response) {
   if (err) {
@@ -20,6 +24,20 @@ app.use(express.static(__dirname + '/public'));
        });
     });   
  });
+
+ // I now know how to send html from expressjs
+ // here is the code below
+ 
+ // app.get('/',function(req,res) {
+ //   messagebird.balance.read(function (err, response) {
+ //  if (err) {
+ //   console.log(err);
+ //  } else
+ //       // res.render({balance:response.amount, type:response.type});
+ //       res.sendFile(__dirname + '/views/index.html', {
+ //        // type:response.type
+ //        balance:response.amount,
+ //        type:response.type
 
 
 
